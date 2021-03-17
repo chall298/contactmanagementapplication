f44222b4dfda761edbbf8686b243f403058bcd3a
Christopher Hall <chall361@gmail.com>
2021-03-17 15:21:24 +0000

    working on PUT method

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -40,6 +40,29 @@ const getState = ({ getStore, setStore }) => {
 						})
 						.catch(e => console.error(e));
 				});
+			},
+			editContact(id, name, phone, email, address) {
+				fetch(url + id, {
+					method: "put",
+					headers: { "Content-type": "application/json" },
+					body: JSON.stringify({
+						full_name: name,
+						phone: phone,
+						address: address,
+						email: email,
+						agenda_slug: "downtown xii"
+					})
+				}).then(() => {
+					fetch(url + "agenda/downtown xii")
+						.then(response => response.json())
+						.then(result => {
+							console.log("result", result),
+								setStore({
+									contacts: result
+								});
+						})
+						.catch(e => console.error(e));
+				});
 			}
 		}
 	};
---
+++ src/js/views/editContact.js
@@ -0,0 +1,77 @@
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+import PropTypes from "prop-types";
+
+export const EditContact = props => {
+	const { store, actions } = useContext(Context);
+	const [phone, setPhone] = useState("");
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [address, setAddress] = useState("");
+
+	console.log("try", store.contacts);
+
+	return (
+		<div className="container">
+			<div>
+				<h1 className="text-center mt-5">Edit Contact</h1>
+				<form>
+					<div className="form-group">
+						<label>Full Name</label>
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Full Name"
+							onChange={e => setName(e.target.value)}
+						/>
+					</div>
+					<div className="form-group">
+						<label>Email</label>
+						<input
+							type="email"
+							className="form-control"
+							placeholder="Enter email"
+							onChange={e => setEmail(e.target.value)}
+						/>
+					</div>
+					<div className="form-group">
+						<label>Phone</label>
+						<input
+							type="phone"
+							className="form-control"
+							placeholder="Enter phone"
+							onChange={e => setPhone(e.target.value)}
+						/>
+					</div>
+					<div className="form-group">
+						<label>Address</label>
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Enter address"
+							onChange={e => setAddress(e.target.value)}
+						/>
+					</div>
+					<Link to={"/"}>
+						<button
+							type="button"
+							className="btn btn-primary form-control"
+							onClick={() => {
+								actions.addContact(name, phone, email, address);
+							}}>
+							save
+						</button>
+					</Link>
+					<Link className="mt-3 w-100 text-center" to="/">
+						or get back to contacts
+					</Link>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+EditContact.propTypes = {
+	match: PropTypes.object
+};
